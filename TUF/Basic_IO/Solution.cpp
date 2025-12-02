7e114e53924df2c18783f7efead2765fc6a5b3c9
PrajwalAmbekar <prajwalambekar.231ch039@nitk.edu.in>
2025-12-02 18:41:07 +0530

    Learnt: Taking IO from user

---
+++ TUF/Basic_IO/Solution.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int x, y;
+
+    cout << "Enter the value of x: ";
+    cin >> x;
+
+    cout << "Enter the value of y: ";
+    cin >> y;
+
+    cout << "The value of x is: " << x 
+         << " and the value of y is: " << y << "\n";
+
+    return 0;
+}
