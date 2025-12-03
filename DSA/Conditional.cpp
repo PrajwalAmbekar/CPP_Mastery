96f153711baebc565851fd97b2611a28841581e6
PrajwalAmbekar <prajwalambekar.231ch039@nitk.edu.in>
2025-12-03 19:34:41 +0530

    Learnt : if else statement and solved some basic problems

--- DSA/Conditional.cpp
+++ DSA/Conditional.cpp
@@ -5,42 +5,42 @@ int main()
 {
     // Example of if-else conditional statement
     // Check if a number is positive, negative, or zero
-    // int num;
-    // cout << "Enter the number to be checked : ";
-    // cin >> num;
-    // if(num > 0)
-    // {
-    //     cout  << "The number entered is positive." << endl;
+    int num;
+    cout << "Enter the number to be checked : ";
+    cin >> num;
+    if(num > 0)
+    {
+        cout  << "The number entered is positive." << endl;
 
-    // }
-    // else if (num < 0)
-    // {
-    //     cout << "The number entered is negative. " << endl;
-    // }
-    // else{
-    //     cout << "The number entered is zero. "<< endl;
-    // }
+    }
+    else if (num < 0)
+    {
+        cout << "The number entered is negative. " << endl;
+    }
+    else{
+        cout << "The number entered is zero. "<< endl;
+    }
 
     /* compare given to numbers*/
-    // int a,b;
-    // cout << "enter two numbers to be compared : ";
-    // cin >> a >> b;
-    // if (a > b ){
-    //     cout << "The number a : " << a << " is greater than b : " << b << endl;
-    // }
-    // else if (a < b){
-    //     cout << "The number b : " << b << " is greater than a : " << a << endl;
-    // }
-    // else{
-    //     cout << "Both numbers are equal. " << endl;
-    // }
-    // return 0;
+    int a,b;
+    cout << "enter two numbers to be compared : ";
+    cin >> a >> b;
+    if (a > b ){
+        cout << "The number a : " << a << " is greater than b : " << b << endl;
+    }
+    else if (a < b){
+        cout << "The number b : " << b << " is greater than a : " << a << endl;
+    }
+    else{
+        cout << "Both numbers are equal. " << endl;
+    }
+    return 0;
 
     /*  difference between cin and cin.get()*/
-    // int a;
-    // a=cin.get();//reads a single character and returns its ASCII value including whitespace and newline characters and stores it in variable a.
-    // cout << "The ASCII value of the character entered is : " << a << endl;
-    // return 0;
+    int a;
+    a=cin.get();//reads a single character and returns its ASCII value including whitespace and newline characters and stores it in variable a.
+    cout << "The ASCII value of the character entered is : " << a << endl;
+    return 0;
 
 
     /* Check given character is lowercase or uppercase or digit*/
