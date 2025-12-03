558af73022321a577c1259cf22a3b604d374ce82
PrajwalAmbekar <prajwalambekar.231ch039@nitk.edu.in>
2025-12-03 19:43:53 +0530

    Learnt : while loops for sum of natural numbers,check prime number or not

---
+++ DSA/Loops.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+    /*Sum of all numbers upto the given number*/
+    // int n;
+    // cout << "Enter a number : ";
+    // cin >> n;   
+    // int sum = 0;
+    // int i = 1;
+    // while(i <= n){
+    //     sum += i;
+    //     i++;
+    // }
+    // cout << "The sum of all numbers upto " << n << " is : " << sum << endl;
+    // return 0;
+
+    /* Prime number*/
+    // int n;
+    // cout << "Enter a number to check if it is prime or not : "; 
+    // cin >> n;
+    // bool isPrime = true;
+    // for (int i = 2; i <= n/2; i++){
+    //     if (n % i == 0){
+    //         isPrime = false;
+    //         break;
+    //     }
+    // }
+    // if (isPrime && n > 1){
+    //     cout << n << " is a prime number. " << endl;
+    // }
+    // else{
+    //     cout << n << " is not a prime number. " << endl;
+    // }
+
+    /* Using while loop*/
+    // int i = 2;
+    // while(i < n){
+    //     if (n % i == 0){
+    //         cout << n << " is not a prime number. " << endl;
+    //         return 0;
+    //     }
+    //     i++;
+    // }
+    // cout << n << " is a prime number. " << endl;
+    // return 0;
+
+    /**/
+}
